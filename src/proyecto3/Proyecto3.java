e517b8e4e4237f8526b4fe155eea93a4662a8dca
ddiazghub <dedemoya@uninorte.edu.co>
2021-03-06 14:47:07 -0500

    first commit

---
+++ src/proyecto3/Proyecto3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto3;
+
+/**
+ *
+ * @author david
+ */
+public class Proyecto3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        captura c = new captura();
+        c.setBounds(10, 10, 500, 500);
+        c.setVisible(true);
+    }
+}
